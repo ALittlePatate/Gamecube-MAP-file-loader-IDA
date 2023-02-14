2b6848308eeaa509e995d7ce5c500a21f0e0a811
ALittlePatate <48603993+ALittlePatate@users.noreply.github.com>
2023-02-14 15:22:14 +0100

    fix: invalid names, askopenfilename

--- load_map.py
+++ load_map.py
@@ -1,13 +1,14 @@
 import ida_search
 import idc
-import tkinter.filedialog
+import tkinter as tk
+from tkinter import filedialog
 
 def rename_func(addr, function_name) : 
     if addr == idc.BADADDR :
         print("Bad address")
         return
     
-    if not idc.set_name(int(addr, 16), function_name, 0x800) : #the 0x800 flag is for "FORCE", so it will rename the function even if it already has a name.
+    if not idc.set_name(int(addr, 16), function_name): #the 0x800 flag is for "FORCE", so it will rename the function even if it already has a name --> no using it cuz it renames functions multiple times.
         print("Rename address failed")
         return
     
@@ -28,15 +29,17 @@ def read_map(file_path) :
                 line = line[18:] #8040e570  4 @354 	JSystem.a JALCalc.cpp
                 data = line.split(" ")
                 addr = data[0]
-                name = data[3]
+                name = data[3].replace("sub","")
 
                 if "." in addr : continue
+                if " " in name or "\t" in name : continue
 
                 rename_func(addr, name)
 
 print("[+] Starting...")
+root = tk.Tk()
+root.withdraw()
 file_path = filedialog.askopenfilename()
-
 print("[+] Scanning map file")
 read_map(file_path)
 print("[+] Done !")
